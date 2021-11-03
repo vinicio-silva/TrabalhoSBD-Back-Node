a84015cc82e1c9ec614242b924968c7a5d34bf2f
Vinicio <viniciobernardessilva@gmail.com>
2021-11-03 01:39:18 -0300

    Commit

--- bd.js
+++ bd.js
@@ -2,7 +2,7 @@ const Pool = require('pg').Pool;
 
 const pool = new Pool({
   user: "postgres",
-  password: "Dml2009123abc",
+  password: "",
   host: "localhost",
   port: 5432,
   database: "bd_universidade"
